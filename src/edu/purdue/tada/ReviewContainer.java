24496acac1913a81e0c063fefcef96874b68ec01
Ben Klutzke <bklutzke@purdue.edu>
2015-01-26 19:01:00 -0500

    ReviewList items implement comparable and are sorted to make most recent first. BLD thing. Style tests.

--- src/edu/purdue/tada/ReviewContainer.java
+++ src/edu/purdue/tada/ReviewContainer.java
@@ -1,6 +1,10 @@
 package edu.purdue.tada;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 /**
  * Review activity list is split into sections. This class details the contents
@@ -10,21 +14,29 @@ import java.util.ArrayList;
  * 
  */
 
-public class ReviewContainer {
-	private String date;
+public class ReviewContainer implements Comparable<ReviewContainer>{
+	private Date date;
 	private ArrayList<ReviewItem> items;
 	
 	public ReviewContainer(String d){
-		this.date = d;
+		try {
+			this.date = new SimpleDateFormat("MMMM, yyyy", Locale.ENGLISH).parse(d);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			this.date = null;
+		}
 		items = new ArrayList<ReviewItem>();
 	}
 	
 	public void addItem(ReviewItem ri){
 		items.add(ri);
 	}
-	
-	public String getDate(){
+	public Date getDate(){
 		return date;
+	}	
+	public String getDateString(){
+		return new SimpleDateFormat("MMMM, yyyy").format(date).toString();
 	}
 	
 	public ArrayList<ReviewItem> getItems(){
@@ -33,5 +45,10 @@ public class ReviewContainer {
 	
 	public int getItemCount(){
 		return items.size();
+	}
+
+	@Override
+	public int compareTo(ReviewContainer rc) {
+		return date.compareTo(rc.getDate());
 	}
 }
