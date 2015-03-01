38df95392d85787ca9ca4a9e50416b37b04cd493
Nicole Missele <nmissele@purdue.edu>
2015-02-28 23:14:23 -0500

    Buttons on the Research login screen are functional.  Pressing submit will take user to ReasearchSettings.
    Created new ResearchSettings activity class where reminders can be set.
    That functionality is not operational yet.
    
    Added ResearchSettings to AndroidManifest.

--- src/edu/purdue/tada/ResearchPassword.java
+++ src/edu/purdue/tada/ResearchPassword.java
@@ -1,6 +1,11 @@
 package edu.purdue.tada;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class ResearchPassword extends BaseActivity{
@@ -9,8 +14,45 @@ public class ResearchPassword extends BaseActivity{
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.password_layout);
+		
+		//Define buttons Nicole Missele 2/28/15
+		Button btncancel = (Button)findViewById(R.id.pwd_cancel);
+		Button btnsubmit = (Button)findViewById(R.id.pwd_submit);
+		
+		//Define cancel button
+		btncancel.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(ResearchPassword.this, SettingsActivity.class)
+				.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				Window w = SettingsGroup.group.getLocalActivityManager()
+						.startActivity("SettingsActivity", intent);
+				View view = w.getDecorView();
+				SettingsGroup.group.setContentView(view);
+				}
+			});
+		
+		//Define the submit button
+		btnsubmit.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(ResearchPassword.this, ResearchSettings.class)
+				.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				Window w = SettingsGroup.group.getLocalActivityManager()
+						.startActivity("ResearchSettings", intent);
+				View view = w.getDecorView();
+				SettingsGroup.group.setContentView(view);
+				}
+			});
+		
+		
+		
+		
+		}
 	}
-}
+
 
 		
 
---
+++ src/edu/purdue/tada/ResearchSettings.java
@@ -0,0 +1,12 @@
+package edu.purdue.tada;
+
+import android.os.Bundle;
+
+public class ResearchSettings extends BaseActivity{
+	
+	//open the research settings from the password screen Nicole Missele 2/28/15
+		public void onCreate(Bundle savedInstanceState){
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.research_settings);
+		}
+}
