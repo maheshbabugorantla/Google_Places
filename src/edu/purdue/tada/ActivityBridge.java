ffb26271b556bdd9328aaa4bfc7862d39e4b1c99
Siyang Chen <chen1564@purdue.edu>
2015-04-04 15:21:34 -0400

    move scan button to the camera page

--- src/edu/purdue/tada/ActivityBridge.java
+++ src/edu/purdue/tada/ActivityBridge.java
@@ -31,6 +31,7 @@ public class ActivityBridge {
 	private boolean checked1 = true;
 	private boolean checked2 = true;
 	private boolean checked3 = true;
+	private boolean checked4 = false;
 	private boolean radio0 = true;
 	private boolean radio1 = false;
 	private boolean radio2 = false;
@@ -129,6 +130,15 @@ public class ActivityBridge {
 	public boolean getChecked3() {
 		return checked3;
 	}
+	
+	public void setChecked4() {
+		if(checked4){checked4 = false;}
+		else {checked4 = true;}
+	}
+	
+	public boolean getChecked4() {
+		return checked4;
+	}	
 	
 	public boolean isRadio0() {
 		return radio0;
